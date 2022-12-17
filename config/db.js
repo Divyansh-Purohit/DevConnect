b10177b9adf5512fda6997c04858e40798397ced
Divyansh Purohit <dpurohit_be19@thapar.edu>
2022-12-17 19:36:56 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mongoURI');
+const db = "mongodb+srv://Divyansh:Siddharth%401993@cluster0.qgdsikw.mongodb.net/?retryWrites=true&w=majority";
 
 const connectDB = async () => {
   try {
